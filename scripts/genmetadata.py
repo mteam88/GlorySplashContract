46213154463b412efdb287a43e67135d3529b04f
mteam88 <matthew8edelen@gmail.com>
2022-06-02 13:32:49 +0000

    awesome

--- scripts/genmetadata.py
+++ scripts/genmetadata.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 INPUTFILE = "metadata.json"
 OUTPUTDIRECTORY = "metadata/"
@@ -7,11 +8,22 @@ class FullMD(list):
     pass
 
 class Metadata(dict):
-    def __init__(self, mdid, name, description, filetype=".jpg"):
+    def __init__(self, mdid, name, filetype=".jpg"):
         self["mdid"] = mdid
         self["name"] = name
         self["image"] = f"ipfs://QmXsEyW6vNFrjLmiBcJFCA2LDy53jnPRLFPnib8Z5HYqXd/{mdid}{filetype}"
-        self["description"] = description
+        self["description"] = self.getdescriptionfromname(name)
+
+    @staticmethod
+    def getdescriptionfromname(name):
+        r = requests.post(
+            "https://api.deepai.org/api/text-generator",
+            data={
+                'text': name,
+            },
+            headers={'api-key': 'd60d0680-40dc-4dcf-a61d-9dc7a697ce4e'}
+        )
+        return r.json()['output']
 
 md = FullMD()
 
@@ -19,7 +31,7 @@ def generatemd(md):
     with open(INPUTFILE) as jsonfile:
         metadatainfo = json.load(jsonfile)
         for mdnum in range(len(metadatainfo["nfts"])):
-            md.append(Metadata(mdnum, metadatainfo["nfts"][mdnum]["name"], metadatainfo["nfts"][mdnum]["description"]))
+            md.append(Metadata(mdnum, metadatainfo["nfts"][mdnum]["name"]))
 
 generatemd(md)
 
