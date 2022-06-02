c177939668ba2074f394d6852129ffd3cab99c73
mteam88 <matthew8edelen@gmail.com>
2022-06-02 15:46:19 +0000

    best practices

--- contracts/GlorySplashes.sol
+++ contracts/GlorySplashes.sol
@@ -1,12 +1,14 @@
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.9;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import "hardhat/console.sol";
 
 contract GlorySplashes is ERC1155 {
     uint256 public constant MAXID=14;
 
-    constructor() public ERC1155("ipfs://QmWQYDLsNQh7PuT4bNUvYHgSoF3bbQMUALDhQcvGViy968/{id}.json") {
+    constructor() ERC1155("ipfs://QmWQYDLsNQh7PuT4bNUvYHgSoF3bbQMUALDhQcvGViy968/{id}.json") {
         for (uint256 i=0; i<15; i++) {
         _mint(msg.sender, i, 1, "");
         }
